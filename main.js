d1cfd3f0a2afff937e8fa480c5178dfe81546edd
Alpha Centaurus <alphacentaurus706@gmail.com>
2025-04-13 10:26:26 +0100

    Added code

--- main.js
+++ main.js
@@ -34,3 +34,54 @@ navLinks.forEach((link) => {
     link.classList.add("active");
   });
 });
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("mouseover", function (e) {
+    this.style.color = "#3498db";
+  });
+
+  link.addEventListener("mouseout", function (e) {
+    this.style.color = "#555";
+  });
+});
+// document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+//   document.body.classList.add('dark-mode');
+//   document.body.classList.remove('light-mode');
+// });
+
+// document.getElementById('light-mode-toggle').addEventListener('click', function() {
+//   document.body.classList.add('light-mode');
+//   document.body.classList.remove('dark-mode');
+// });
+// const themeToggle = document.getElementById("theme-toggle");
+// const body = document.body;
+// const navbar = document.querySelector(".navbar");
+// const header = document.querySelector(".header");
+// const main = document.querySelector("main");
+
+// themeToggle.addEventListener("click", () => {
+//   body.classList.toggle("dark-mode");
+//   navbar.classList.toggle("dark-mode");
+//   header.classList.toggle("dark-mode");
+//   main.classList.toggle("dark-mode");
+
+//   if (body.classList.contains("dark-mode")) {
+//    localStorage.setItem("theme", "dark");
+//   } else {
+//    localStorage.setItem("theme", "light");
+//   }
+// });
+
+// if (typeof localStorage !== "undefined") {
+//   const savedTheme = localStorage.getItem("theme");
+//   if (savedTheme === "dark") {
+//     body.classList.add("dark-mode");
+//     navbar.classList.add("dark-mode");
+//     header.classList.add("dark-mode");
+//     main.classList.add("dark-mode");
+//   } else if (savedTheme === "light") {
+//     body.classList.remove("dark-mode");
+//     navbar.classList.remove("dark-mode");
+//     header.classList.remove("dark-mode");
+//     main.classList.remove("dark-mode");
+//   }
+// }
