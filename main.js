eb847b38cb23f11076a78b7534e4bd2059076079
Alpha Centaurus <alphacentaurus706@gmail.com>
2025-05-25 19:37:53 +0100

    feat: enhance fullscreen navigation with updated styles and functionality

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector(".nav-links").classList.toggle("active");
+});
+const hamburger = document.querySelector(".hamburger");
+const fullscreenNav = document.getElementById("myFullscreenNav");
+const closeButton = document.querySelector(".closebtn");
+
+hamburger.addEventListener("click", () => {
+  fullscreenNav.style.width = "100%";
+});
+
+closeButton.addEventListener("click", () => {
+  fullscreenNav.style.width = "0%";
+});
 
