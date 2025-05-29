42b2a55ae1ff3a18da4c4299052fbfd006a265e5
TheAtlasCorrelation <alphacentaurus706@gmail.com>
2025-05-29 13:29:36 +0000

    fix: refine hamburger menu toggle functionality and update styles for better visibility

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger");
   const fullscreenNav = document.getElementById("myFullscreenNav");
-  const closeButton = document.querySelector(".hamburger.active");
 
   hamburger.addEventListener("click", function () {
-    hamburger.classList.add("active");
-    fullscreenNav.style.width = "100%";
-  });
-
-  closeButton.addEventListener("click", function () {
-    fullscreenNav.style.width = "0%";
-    hamburger.classList.remove("active");
+    if (!hamburger.classList.contains("active")) {
+      hamburger.classList.add("active");
+      fullscreenNav.style.width = "100%";
+    } else {
+      fullscreenNav.style.width = "0%";
+      hamburger.classList.remove("active");
+    }
   });
 });
